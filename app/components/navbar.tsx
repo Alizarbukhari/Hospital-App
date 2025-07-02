1e8b5c91e8a56912318c1c5086c5d42bd7afc284
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-07-02 10:45:07 +0500

    initialize The project

---
+++ app/components/navbar.tsx
@@ -0,0 +1,16 @@
+
+
+function Navbar() {
+  return (
+    <div><div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div></div>
+  )
+}
+
+export default Navbar
